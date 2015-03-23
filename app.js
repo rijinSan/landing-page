5239d55d8875ace58bb336c3761ff4b0f6836ba2
rijin t <rijint@prohaktiv.com>
2015-03-23 23:04:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+/*
+ * Module dependencies
+ */
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib')
+
+
+var app = express()
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+  res.render('home',
+  { title : 'Home' }
+  )
+})
+app.get('/home', function (req, res) {
+  res.render('home',
+  { title : 'Home' }
+  )
+})
+console.log("server started on port 4000"); 
+app.listen(4000)
